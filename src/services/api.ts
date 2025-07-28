88c30322a44c61dc97841d9894645ece227f0108
Gustavo Bruno <gugubrunopf@gmail.com>
2025-07-28 08:45:37 -0300

    refactor: changed background video from ./src to public/ feat: new function to search a product by id in ./api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 // Funções para enviar requisições às API's externas.
 
+// Função para autenticar usuário
+// Recebe um nome de usuário e senha, retorna os dados do usuário autenticado.
 export const UserLogin = async (username: string, password: string) =>{
     
     const response = await fetch('https://dummyjson.com/auth/login', {
@@ -18,10 +20,26 @@ export const UserLogin = async (username: string, password: string) =>{
 
 };
 
+// Função para buscar produtos da API
+// Recebe um limite e um deslocamento (skip) para paginação.
 export const getProductsAPI = async(limit: number, skip: number) =>{
 
     const response = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`,)
     
-    return await response.json()
+    return await response.json();
+
+}
+
+// Função para buscar detalhes de um produto específico
+// Recebe o ID do produto.
+export const getProductAPI = async(id: string) =>{
+
+    const response = await fetch(`https://dummyjson.com/products/${id}`,)
+
+    if (!response.ok) {
+        throw new Error('Produto não encontrado');
+    }
+
+    return await response.json();
 
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,9 @@
 // types.ts (novo arquivo de tipagem)
+
+// Tipos de dados para o sistema de e-commerce
+
+// interface Product 
+// Define a estrutura de um produto no e-commerce -> Dados retornados da API.
 export interface Product {
   id: string;
   title: string;
